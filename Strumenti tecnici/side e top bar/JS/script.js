c988cbfb1644d2f87214ec7fa182565060fe96a4
Vittorio <vittofv01@gmail.com>
2023-01-20 15:07:45 +0100

    Aggiungo file utili

---
+++ Strumenti tecnici/side e top bar/JS/script.js
@@ -0,0 +1,15 @@
+if (screen.width > screen.height){
+  document.querySelector("body").classList.add("desktop");
+}
+
+function openNav() {
+  document.getElementById("sidebar").style.width = "80vw";
+  document.getElementById("close").style.display = "block";
+  document.getElementById("close").style.opacity = "20%";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("close").style.display = "none";
+  document.getElementById("close").style.opacity = "0%";
+}
