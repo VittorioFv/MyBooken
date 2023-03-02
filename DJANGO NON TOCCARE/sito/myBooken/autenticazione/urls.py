713b92932e5339fbbbb44516562b60e88da1302d
Vittorio <vittofv01@gmail.com>
2023-03-02 14:49:25 +0100

    Legistrazione, login, logout funzionanti

--- DJANGO NON TOCCARE/sito/myBooken/autenticazione/urls.py
+++ DJANGO NON TOCCARE/sito/myBooken/autenticazione/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginUtente, name='login')
+    path('login/', views.loginUtente, name='login'),
+    path('logoutUtente/', views.logoutUtente, name='logout'),
+    path('registrazione/', views.registrazioneUtente, name='registrazione'),
 ]
--- DJANGO NON TOCCARE/sito/myBooken/autenticazione/views.py
+++ DJANGO NON TOCCARE/sito/myBooken/autenticazione/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import formDiRegistrazione
 
 def loginUtente(request):
     if request.method == 'POST':
@@ -15,3 +17,26 @@ def loginUtente(request):
             return redirect('login')
     else:
         return render(request, 'login.html')
+
+def logoutUtente(request):
+    logout(request)
+    return render(request, 'login.html')
+
+def registrazioneUtente(request):
+    if request.method == 'POST':
+        form = formDiRegistrazione(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            
+            utente = authenticate(username = username, password = password)
+            login(request, utente)
+
+            messages.success(request, (" Sei stato registrato correttamente"))
+            return redirect('libri')
+    else:
+        form = formDiRegistrazione()
+    return render(request, 'registrazione.html', {
+        'form': form,
+    })
