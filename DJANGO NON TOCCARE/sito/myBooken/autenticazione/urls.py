4c6f658b50f2a8590b5f7795e7183423fd819b8d
vitto <vittofv01@gmail.com>
2023-03-03 00:31:52 +0100

    verifica mail
    manca limitare l'accesso, sistemare la grafica (collegando messages e errors), verificare che le mail siano diverse, eliminare l'account inattivo dopo un tot

--- DJANGO NON TOCCARE/sito/myBooken/autenticazione/urls.py
+++ DJANGO NON TOCCARE/sito/myBooken/autenticazione/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('attiva/<uidb64>/<token>', views.attivaUtente, name='attiva'),
     path('login/', views.loginUtente, name='login'),
     path('logoutUtente/', views.logoutUtente, name='logout'),
     path('registrazione/', views.registrazioneUtente, name='registrazione'),
--- DJANGO NON TOCCARE/sito/myBooken/myBooken/settings.py
+++ DJANGO NON TOCCARE/sito/myBooken/myBooken/settings.py
@@ -126,3 +126,15 @@ LOGOUT_REDIRECT_URL = "/login"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Verifica email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FORM = 'MyBooken.noreply@gmail.com'
+EMAIL_HOST_USER = 'MyBooken.noreply@gmail.com'
+EMAIL_HOST_PASSWORD = 'ptxtjersvskxthtu'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+PASSWORD_RESET_TIMEOUT = 14400
