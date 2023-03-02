45709fb72503c2550a57a7bd4caa872747564999
Vittorio <vittofv01@gmail.com>
2023-03-02 14:50:58 +0100

    .

--- DJANGO NON TOCCARE/sito/myBooken/autenticazione/forms.py
+++ DJANGO NON TOCCARE/sito/myBooken/autenticazione/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 class formDiRegistrazione(UserCreationForm):
     email = forms.EmailField()
-    
+
     class Meta:
         model = User
         fields = ('username','email','password1','password2')
