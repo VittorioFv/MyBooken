2e35fd9add469b57962aaf1d5ff154b5655abe1d
Vittorio <vittofv01@gmail.com>
2023-03-02 15:20:08 +0100

    aggiunta classi css ai field dei form della registrazione

--- DJANGO NON TOCCARE/sito/myBooken/autenticazione/forms.py
+++ DJANGO NON TOCCARE/sito/myBooken/autenticazione/forms.py
@@ -3,8 +3,16 @@ from django.contrib.auth.models import User
 from django import forms
 
 class formDiRegistrazione(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField()#widget = forms.EmailInput(attrs = {'class':'form-coontrol'})) #per qualche motivo non funziona 
 
     class Meta:
         model = User
         fields = ('username','email','password1','password2')
+
+    def __init__(self, *args, **kwargs):
+        super(formDiRegistrazione, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
