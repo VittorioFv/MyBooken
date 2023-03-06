c02f2d88d05f812ee39b706bc2a0d859f5a89882
Vittorio <vittofv01@gmail.com>
2023-03-06 12:45:17 +0100

    .

--- DJANGO NON TOCCARE/sito/myBooken/myBooken/settings.py
+++ DJANGO NON TOCCARE/sito/myBooken/myBooken/settings.py
@@ -131,9 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Verifica email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FORM = 'MyBooken.noreply@gmail.com'
-EMAIL_HOST_USER = 'MyBooken.noreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'ptxtjersvskxthtu'
+EMAIL_FORM = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
