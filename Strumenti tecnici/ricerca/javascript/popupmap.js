ebfe95be75eef2034a7244ef7bfe37b257c87e41
FreireGabri <123184432+FreireGabri@users.noreply.github.com>
2023-03-09 14:19:07 +0100

    aggiunta alert

--- Strumenti tecnici/ricerca/javascript/popupmap.js
+++ Strumenti tecnici/ricerca/javascript/popupmap.js
@@ -7,3 +7,7 @@ document.querySelector("#show-modal-btn").addEventListener("click", () => {
 document.querySelector("#close-modal-btn").addEventListener("click", () => {
     overlay.style.display = 'none';
 })
+
+function myalert() {
+    alert("La posizione è indicativa del paese nel quase si trova il libro.");
+}
