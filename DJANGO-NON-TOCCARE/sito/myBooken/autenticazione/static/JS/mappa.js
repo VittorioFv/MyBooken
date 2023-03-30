129b2eb608259b4fa093149019ee051b8071c149
Vittorio <vittofv01@gmail.com>
2023-03-30 12:35:02 +0200

    Recupero password

--- DJANGO-NON-TOCCARE/sito/myBooken/autenticazione/static/JS/mappa.js
+++ DJANGO-NON-TOCCARE/sito/myBooken/autenticazione/static/JS/mappa.js
@@ -6,6 +6,7 @@ var markersArray = []
 /********************* CREA LA MAPPA ************************/
 map = new OpenLayers.Map("basicMap");
 var mapnik = new OpenLayers.Layer.OSM();
+
 var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
 var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 var position = new OpenLayers.LonLat(10.9, 45.5).transform(fromProjection, toProjection);
--- DJANGO-NON-TOCCARE/sito/myBooken/autenticazione/urls.py
+++ DJANGO-NON-TOCCARE/sito/myBooken/autenticazione/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('attiva/<uidb64>/<token>', views.attivaUtente, name='attiva'),
+    path('recupera_password/<uidb64>/<token>', views.recuperaPassword, name='cambia_password'),
+
+    path('recupera_password', views.passwordDimenticata, name='recupera'),
     path('login/', views.loginUtente, name='login'),
     path('logout_utente/', views.logoutUtente, name='logout'),
     path('registrazione/', views.registrazioneUtente, name='registrazione'),
