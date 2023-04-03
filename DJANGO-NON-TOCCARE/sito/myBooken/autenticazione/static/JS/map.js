dbab46bf0aaed005409ecf8d117b93d84aae7312
zeno_begnini <zeno.begnini@gmail.com>
2023-04-03 10:43:18 +0200

    map 2.1.2
    Capire perchè da problemi con certi userrname e password e capire come fare un controllo

--- DJANGO-NON-TOCCARE/sito/myBooken/autenticazione/static/JS/map.js
+++ DJANGO-NON-TOCCARE/sito/myBooken/autenticazione/static/JS/map.js
@@ -25,9 +25,12 @@ function cercaCitta(cittaDaCercare) {
       var lon = data[0].lon;
       map.setView(new L.LatLng(lat, lon), 13);
       currentMarker = L.marker([lat, lon]).addTo(map);
-      map.fitBounds(marker.getBounds());
-      document.getElementById("longitudine").value = lng;
+      //map.fitBounds(marker.getBounds());
+      document.getElementById("longitudine").value = lon;
       document.getElementById("latitudine").value = lat;
+      console.log("Quello che mi interessa: "+ document.getElementById("longitudine").value);
+      console.log("Quello che mi interessa: "+ document.getElementById("latitudine").value);
+
       document.getElementById('basicMap').style.left = "20%";
       document.getElementById('overlay').style.display = "block";
       document.getElementById('bottoneRicerca').style.display = "block";
--- DJANGO-NON-TOCCARE/sito/myBooken/myBooken/settings.py
+++ DJANGO-NON-TOCCARE/sito/myBooken/myBooken/settings.py
@@ -140,7 +140,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FORM = 'mybooken.noreply@gmail.com'
 EMAIL_HOST_USER = 'mybooken.noreply@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'bodokofyrzdldalb'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
