469c9300ae6a24a40e9d94ae44b4da758325d346
zeno_begnini <zeno.begnini@gmail.com>
2023-04-20 17:06:12 +0200

    bestemmia dopo bestemmia

--- DJANGO-NON-TOCCARE/sito/myBooken/libri/static/JS/agglibro.js
+++ DJANGO-NON-TOCCARE/sito/myBooken/libri/static/JS/agglibro.js
@@ -2,8 +2,11 @@ const overlay = document.querySelector("#overlaybook");
 
 document.querySelector("#show-modalbook-btn").addEventListener("click", () => {
     overlay.style.display = 'block';
+    modalbook.style.display = 'block';
 })
 
 document.querySelector("#close-modalbook-btn").addEventListener("click", () => {
     overlay.style.display = 'none';
+    modalbook.style.display = 'none';
+
 })
