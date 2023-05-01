b0fa7ffab4c5707f9c842a0e77b06299d138ecc5
zeno_begnini <zeno.begnini@gmail.com>
2023-05-01 17:01:16 +0200

    Risolto problema genera codice

--- DJANGO-NON-TOCCARE/sito/myBooken/interazioniUtenti/urls.py
+++ DJANGO-NON-TOCCARE/sito/myBooken/interazioniUtenti/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.chatMain, name='chat_main'),
     path('codice/<int:codiceScambio>/', views.controllaCodiceScambio, name='controlla_codice'),
     path('scambia_libro/<str:username>', views.scegliLibro, name='scambia_libro'),
-    path('scambia_libro/genera_codice/<int:id>/', views.generaCodice, name='genera_codice')
+    path('scambia_libro/genera_codice/<int:id>', views.generaCodice, name='genera_codice')
 ]
